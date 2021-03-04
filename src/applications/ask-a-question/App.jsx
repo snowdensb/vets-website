2bc8c7640e0a5ec0bc3a1b62d9a0599de93dfdc2
Paul Phillips <paul.phillips@thoughtworks.com>
2021-03-04 15:52:24 -0600

    Example of how to use the es5 bundle and share React+ReactDOM with it

--- src/applications/ask-a-question/App.jsx
+++ src/applications/ask-a-question/App.jsx
@@ -1,21 +1,13 @@
-import React, { useMemo } from 'react';
-import ReactWebChat, {
-  createDirectLine,
-  createStore,
-} from 'botframework-webchat';
+import React, { useMemo, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+window.React = React;
+window.ReactDOM = ReactDOM;
 
 const BotActivityDecorator = ({ children }) => {
   return (
     <div className="botActivityDecorator">
-      <ul className="botActivityDecorator__buttonBar">
-        <li>
-          <button className="botActivityDecorator__button">yes</button>
-        </li>
-        <li>
-          <button className="botActivityDecorator__button">no</button>
-        </li>
-      </ul>
-      <div className="botActivityDecorator__content">{children}</div>
+        {children}
     </div>
   );
 };
@@ -36,14 +28,31 @@ const activityMiddleware = () => next => (...setupArgs) => {
 };
 
 export default function App() {
+  const [isLoaded, setLoaded] = useState(!!window.WebChat);
+  
+  if(!isLoaded) {
+    setTimeout(() => {
+      if(window.WebChat) {
+        setLoaded(true);     
+      }
+    }, 300)
+    return 'waiting on webchat framework . . .'
+  }
+
+  return <ActualApp/>;
+}
+
+function ActualApp() {
+  const { ReactWebChat, createDirectLine } = window.WebChat;
+
   const store = useMemo(() => createStore(), []);
   const directLine = useMemo(
     () =>
       createDirectLine({
-        domain:
-          'https://northamerica.directline.botframework.com/v3/directline',
+        // domain:
+        //   'https://northamerica.directline.botframework.com/v3/directline',
         token:
-          'ew0KICAiYWxnIjogIlJTMjU2IiwNCiAgImtpZCI6ICJsY0oxTXFpNkdKYXdCZEw5Y0dieEt5S1R6OE0iLA0KICAieDV0IjogImxjSjFNcWk2R0phd0JkTDljR2J4S3lLVHo4TSIsDQogICJ0eXAiOiAiSldUIg0KfQ.ew0KICAiYm90IjogImF6Y2N0b2xhYmhlYWx0aGJvdC1kamVvZXhjIiwNCiAgInNpdGUiOiAiUHh4SzRLa083aVkiLA0KICAiY29udiI6ICJCcDVYUDc5aFRwZ0RzR3NxQWZibGQ1LW8iLA0KICAibmJmIjogMTYxMzU4MTM4OCwNCiAgImV4cCI6IDE2MTM1ODQ5ODgsDQogICJpc3MiOiAiaHR0cHM6Ly9kaXJlY3RsaW5lLmJvdGZyYW1ld29yay5jb20vIiwNCiAgImF1ZCI6ICJodHRwczovL2RpcmVjdGxpbmUuYm90ZnJhbWV3b3JrLmNvbS8iDQp9.y3T08F4cLmvThvTtFGqx6JgJj4Jhh096-gEZi86RJ1Z95BjpMoAe5MD1L8CHsVhrPaySjFJx3HlwrB0qJwfx7LOcNZ7MWQs_rUMUXbdHJYkWwzyWVXLKzJ78BXLVhP-9NQT3aHpaFMLWLM8a6CU4nnmse-xuFBlL5OP7P5brCZMaHp-bD6Zxs5AQvc9sIWSnp9avAOeHe0q5Jv8911QM950ThzsJ6Ru6V0QQSQE3zyj621il8ZINNf8b_26YlFI9cuqZMhVx63s_7no5UF1oynXts18w-quEnse2_AP4FynUUKeZsNgr2INwgyBjf5Xs4RMyjTg0jMDWV26sPZzftg',
+          'ew0KICAiYWxnIjogIlJTMjU2IiwNCiAgImtpZCI6ICJsY0oxTXFpNkdKYXdCZEw5Y0dieEt5S1R6OE0iLA0KICAieDV0IjogImxjSjFNcWk2R0phd0JkTDljR2J4S3lLVHo4TSIsDQogICJ0eXAiOiAiSldUIg0KfQ.ew0KICAiYm90IjogInZhYm90LWF6Y2N0b2xhYi1ib3QiLA0KICAic2l0ZSI6ICJ3b1ZDYXV2RzA2QSIsDQogICJjb252IjogIkt2STE3ZElteER5RmtFYkZGd1J4NU0tZCIsDQogICJuYmYiOiAxNjE0ODk0NDQ5LA0KICAiZXhwIjogMTYxNDg5ODA0OSwNCiAgImlzcyI6ICJodHRwczovL2RpcmVjdGxpbmUuYm90ZnJhbWV3b3JrLmNvbS8iLA0KICAiYXVkIjogImh0dHBzOi8vZGlyZWN0bGluZS5ib3RmcmFtZXdvcmsuY29tLyINCn0.hIRwC4lMhwzDrsbtkgBLZE100m6oDtvRp9spjAr8uJYe3QD2_xSA3AHABirtBe5CtVBBM7q373w2VSGrCZDs179CBLJUCCZAzcanagzaAgLyqEY8MBNSzBnjUDqtSFF_EVza_uOL7RhWoto-lClJUQm5lkf-49Oc0CrmgsA2qKc9urm-C7KR2Of24ZS8mqOrzw4k2pI3tZWrYg9SNsgZtwqR-ELK9Nt2WyTcm6FfDX2qW9RjSiKMWPG81IBfnkeWA92EZbQL_D9aHmjIYbfb2yVLmHwTwMSE6xiTeueTLDiygNSo1Tz30MDTAF0DGSDatrYKN1kbwl5kAYXxTGhGiw'
       }),
     [],
   );
