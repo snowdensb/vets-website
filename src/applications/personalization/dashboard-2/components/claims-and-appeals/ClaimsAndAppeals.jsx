2b6681e19059ffcab5e16d1605587cb4356675c6
Erik P. Hansen <eph@erikphansen.com>
2021-03-08 18:47:32 -0800

    more pretty...

--- src/applications/personalization/dashboard-2/components/claims-and-appeals/ClaimsAndAppeals.jsx
+++ src/applications/personalization/dashboard-2/components/claims-and-appeals/ClaimsAndAppeals.jsx
@@ -41,7 +41,23 @@ const HighlightedClaimAppeal = ({ claimOrAppeal }) => {
     return (
       <div className="vads-l-col--12 medium-screen:vads-l-col--6 small-desktop-screen:vads-l-col--8 medium-screen:vads-u-padding-right--3">
         <div className="vads-u-padding-y--2p5 vads-u-padding-x--2p5 vads-u-background-color--gray-lightest">
-          <p>Show some details about the claim or appeal</p>
+          <h3 className="vads-u-margin-top--0">
+            Claim for compensation received June 7, 1999
+          </h3>
+          <div className="vads-u-display--flex">
+            <i
+              aria-hidden="true"
+              className={`fas fa-fw fa-check-circle vads-u-margin-right--1 vads-u-margin-top--0p5 vads-u-color--green`}
+            />
+            <div>
+              <p className="vads-u-margin-y--0">
+                Status: Evidence gathering, review, and decision
+              </p>
+              <p className="vads-u-margin-y--0">
+                We sent you a development letter
+              </p>
+            </div>
+          </div>
           <a className="usa-button-primary" href="claim-or-appeal-status/">
             View details
           </a>
