5c7421c44954558b7fa0f9c4b7033ce685f303ce
Erik P. Hansen <eph@erikphansen.com>
2021-03-08 19:29:55 -0800

    Claim has parity with v1

--- src/applications/personalization/dashboard-2/components/claims-and-appeals/HighlightedClaimAppeal.jsx
+++ src/applications/personalization/dashboard-2/components/claims-and-appeals/HighlightedClaimAppeal.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import moment from 'moment';
 
+import recordEvent from '~/platform/monitoring/record-event';
+import {
+  getPhaseDescription,
+  isClaimComplete,
+  getClaimType,
+} from '~/applications/claims-status/utils/helpers';
 import { appealTypes } from '~/applications/claims-status/utils/appeals-v2-helpers';
+
+const capitalizeFirstLetter = input => {
+  const capitalizedFirstLetter = input[0].toUpperCase();
+  return `${capitalizedFirstLetter}${input.slice(1)}`;
+};
+
+function listPhase(phase) {
+  return phase === 8 ? 'Closed' : getPhaseDescription(phase);
+}
+
+function handleViewClaim() {
+  recordEvent({
+    event: 'dashboard-navigation',
+    'dashboard-action': 'view-button',
+    'dashboard-product': 'view-claim',
+  });
+}
 
 const Appeal = () => {
   return (
@@ -31,12 +55,14 @@ const Appeal = () => {
   );
 };
 
-const Claim = () => {
+const Claim = ({ claim }) => {
+  const inProgress = !isClaimComplete(claim);
+  const dateRecd = moment(claim.attributes.dateFiled).format('MMMM D, YYYY');
   return (
     <div className="vads-l-col--12 medium-screen:vads-l-col--6 small-desktop-screen:vads-l-col--8 medium-screen:vads-u-padding-right--3">
       <div className="vads-u-padding-y--2p5 vads-u-padding-x--2p5 vads-u-background-color--gray-lightest">
         <h3 className="vads-u-margin-top--0">
-          Claim for compensation received June 7, 1999
+          {capitalizeFirstLetter(getClaimType(claim))} claim received {dateRecd}
         </h3>
         <div className="vads-u-display--flex">
           <i
@@ -45,14 +71,29 @@ const Claim = () => {
           />
           <div>
             <p className="vads-u-margin-y--0">
-              Status: Evidence gathering, review, and decision
+              {listPhase(claim.attributes.phase)}
             </p>
-            <p className="vads-u-margin-y--0">
-              We sent you a development letter
-            </p>
+            {inProgress && claim.attributes.developmentLetterSent ? (
+              <p className="vads-u-margin-y--0">
+                We sent you a development letter
+              </p>
+            ) : null}
+            {claim.attributes.decisionLetterSent ? (
+              <p className="vads-u-margin-y--0">
+                We sent you a decision letter
+              </p>
+            ) : null}
+            {inProgress && claim.attributes.documentsNeeded ? (
+              <p className="vads-u-margin-y--0">Items need attention</p>
+            ) : null}
           </div>
         </div>
-        <a className="usa-button-primary" href="claim-or-appeal-status/">
+        <a
+          className="usa-button-primary"
+          href={`/track-claims/your-claims/${claim.id}/status`}
+          aria-label={`View claim received ${dateRecd}`}
+          onClick={handleViewClaim}
+        >
           View details
         </a>
       </div>
@@ -65,8 +106,8 @@ const HighlightedClaimAppeal = ({ claimOrAppeal }) => {
     return <p>You have no claims or appeals updates in the last 30 days.</p>;
   }
   if (appealTypes.includes(claimOrAppeal.type)) {
-    return <Claim claim={claimOrAppeal} />;
+    return <Appeal appeal={claimOrAppeal} />;
   }
-  return <Appeal appeal={claimOrAppeal} />;
+  return <Claim claim={claimOrAppeal} />;
 };
 export default HighlightedClaimAppeal;
